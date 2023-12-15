f04be4d3923430429b96480368e61b1534717081
novv <novirin002@gmail.com>
2023-12-16 04:16:00 +0700

    1

---
+++ app/src/main/java/com/joba/app/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.joba.app
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var loading: ProgressBar
+    private lateinit var url: String
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setFullscreen()
+        init()
+        setupUI()
+    }
+
+    private fun setFullscreen() {
+        supportActionBar?.hide()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+    private fun init() {
+        url = "https://www.tracyvets.com/"
+        loading = findViewById(R.id.pb_loading)
+        webView = findViewById(R.id.wv_page)
+    }
+
+    private fun setupUI() {
+        if (isOnline(this)) {
+            loadWebview()
+        } else {
+            Toast.makeText(this, "Tidak ada koneksi internet", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("ServiceCast")
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnectedOrConnecting
+    }
+
+    private fun loadWebview() {
+        webView.webViewClient = myWebclient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(url)
+    }
+
+    inner class myWebclient : WebViewClient() {
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            loading.visibility = View.GONE
+        }
+
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
